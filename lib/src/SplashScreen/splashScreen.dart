fab0b2303b3d3b53189f06891195e9234cc0c291
FS.08.03 <202100198@estudantes.ips.pt>
2023-05-24 16:18:10 +0100

    arranjos splash

--- lib/src/SplashScreen/splashScreen.dart
+++ lib/src/SplashScreen/splashScreen.dart
@@ -5,19 +5,20 @@ import 'package:flutter/material.dart';
 import '../HomeScreen/HomeScreen.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key,});
+  const SplashScreen({
+    super.key,
+  });
 
   @override
   // ignore: library_private_types_in_public_api
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-
 class _SplashScreenState extends State {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 1), () {
+    Timer(const Duration(seconds: 2), () {
       Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => const HomeScreen()));
     });
@@ -26,19 +27,19 @@ class _SplashScreenState extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            body: SizedBox(
-              width: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                    Image(image: AssetImage('assets/images/IPSCB_Logo.png'),
-                    width: 300,
-                  ),
-                  SizedBox(height: 50,)
-                ],
-              ),
+      //backgroundColor: Colors.black,
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Image(
+              image: AssetImage('assets/images/IPSCB_Logo1.png'),
+              width: 300,
             )
-         );
+          ],
+        ),
+      ),
+    );
   }
 }
- 
