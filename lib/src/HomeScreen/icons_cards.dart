7f1d4538b398cf65ad761b33d8d99e20f8ed381b
FS.08.03 <202100198@estudantes.ips.pt>
2023-05-31 06:41:58 +0100

    Mais atualizações em vários ecrãs

--- lib/src/HomeScreen/icons_cards.dart
+++ lib/src/HomeScreen/icons_cards.dart
@@ -5,18 +5,21 @@ import 'package:tp_m_ipscb/src/HomeScreen/logoImage.dart';
 import 'package:tp_m_ipscb/src/SplashScreen/splashScreen.dart';
 
 class IconCard extends StatelessWidget {
-  const IconCard({super.key, required this.logoImage});
+  const IconCard({super.key, required this.logoImage,});
 
   final LogoImage logoImage;
-
+  
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: InkWell(
-        child: Image.network(
-          logoImage.logoImage,
-          fit: BoxFit.fitHeight,
-        ), 
+    return Scaffold(
+      body: InkWell(
+        child: Card(
+          child: ListTile(
+            title: Text(logoImage.name,textAlign: TextAlign.center,),
+            contentPadding:const EdgeInsets.symmetric(vertical: 50.0),
+            leading: Icon(logoImage.logoImage,size: 100,), // Ícone de dataset
+          ),
+        ),
         onTap: () {
           Navigator.of(context).push(
             MaterialPageRoute(
@@ -28,4 +31,3 @@ class IconCard extends StatelessWidget {
     );
   }
 }
-
