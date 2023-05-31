fb5fb7f70d20ffd2d33847285fadf7432a7b83bf
FS.08.03 <202100198@estudantes.ips.pt>
2023-05-31 04:15:34 +0100

    Atualizações em homescreen, login, recuPass e regp

--- lib/src/HomeScreen/HomeScreen.dart
+++ lib/src/HomeScreen/HomeScreen.dart
@@ -21,7 +21,7 @@ class HomeScreen extends StatelessWidget {
           onTap: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => LoginPage()),
+              MaterialPageRoute(builder: (context) =>const LoginPageForm()),
             );
           },
           child:const CircleAvatar(
@@ -74,6 +74,16 @@ class HomeScreen extends StatelessWidget {
             Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => const HomeScreen()));
           }
+
+          if (index == 4) {
+            Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+          }
+
+          if (index == 5) {
+            Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+          }
         },
         unselectedItemColor: Colors.blue,
         selectedItemColor: Colors.blueGrey,
@@ -83,17 +93,25 @@ class HomeScreen extends StatelessWidget {
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
             icon: Icon(Icons.settings),
             label: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.info),
             label: 'Credits',
           ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.forum),
+            label: 'Forum',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Salas',
+          )
         ],
       ),
     );
--- lib/src/LoginPage/loginPage.dart
+++ lib/src/LoginPage/loginPage.dart
@@ -1,16 +1,31 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:tp_m_ipscb/src/HomeScreen/homeScreen.dart';
+import 'package:tp_m_ipscb/src/PassRec/PassRec.dart';
+import 'package:tp_m_ipscb/src/RegistoPage/registoPage.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LoginPageForm extends StatefulWidget {
+ const LoginPageForm({super.key});
+
+  @override
+  LoginPage createState() {
+    return LoginPage();
+  }
+}
+
+class LoginPage extends State<LoginPageForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _currentIndex=0;
+
+  
 
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       appBar: PreferredSize(
-        preferredSize:const Size.fromHeight(200), // Defina o tamanho desejado
+        preferredSize: const Size.fromHeight(200), // Defina o tamanho desejado
         child: AppBar(
           backgroundColor: Colors.white,
           flexibleSpace: Column(
@@ -19,20 +34,170 @@ class LoginPage extends StatelessWidget {
               Image.asset(
                 'assets/images/IPSCB_Logo1.png', // Caminho para a imagem
                 height: 150, // Altura da imagem
-                fit: BoxFit.cover, // Ajustar a imagem ao tamanho do espaço disponível
+                fit: BoxFit
+                    .cover, // Ajustar a imagem ao tamanho do espaço disponível
               ),
-             const  SizedBox(height: 5), // Espaçamento entre a imagem e o texto
-               const Text(
+              const SizedBox(height: 5), // Espaçamento entre a imagem e o texto
+              const Text(
                 'INICIAR SESSÃO',
                 style: TextStyle(
-                  color: Colors.blue, // Cor do texto
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold // Tamanho da fonte do texto
-                ),
+                    color: Colors.blue, // Cor do texto
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold // Tamanho da fonte do texto
+                    ),
               ),
             ],
           ),
         ),
-      ));
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey, // Atribuímos a chave do formulário
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'USERNAME',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira o nome';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'PASSWORD',
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira a password';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Lógica para enviar o formulário
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()),
+                    );
+                  }
+                },
+                child: const Text('ENTRAR'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const PassRec()),
+                  );
+                },
+                child: const Text(
+                  'Esqueceu-se da senha? Recuperar senha',
+                  style: TextStyle(
+                      color: Colors.blue, // Cor do texto
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Divider(color: Colors.black),
+              const SizedBox(height: 8),
+              const Text('Ainda não tens uma conta?'),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const RegistoPageForm()),
+                    );
+                  },
+                  child: Column(children: const [
+                    Text('CRIAR CONTA',
+                        style: TextStyle(
+                            color: Colors.blue, // Cor do texto
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold)),
+                  ])),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex, // Define o índice inicial selecionado
+        onTap: (index) {
+          setState(() {
+          });
+          if (index == 0) {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => const HomeScreen()));
+          }
+
+          if (index == 1) {
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const RegistoPageForm()));
+          }
+
+          if (index == 2) {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => const HomeScreen()));
+          }
+
+          if (index == 3) {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => const HomeScreen()));
+          }
+
+          if (index == 4) {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => const HomeScreen()));
+          }
+
+          if (index == 5) {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => const HomeScreen()));
+          }
+        },
+        unselectedItemColor: Colors.blue,
+        selectedItemColor: Colors.blueGrey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'Credits',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.forum),
+            label: 'Forum',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Salas',
+          ),
+        ],
+      ),
+    );
   }
 }
